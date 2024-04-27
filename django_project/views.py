2c1764724ec1ac54e70d534e593869a15a19fde8
ClicheAF83 <159744255+ClicheAF83@users.noreply.github.com>
2024-04-27 20:18:51 +0000

    HTML files added for the navigation. Weather API used to calculate probability it rains

--- django_project/views.py
+++ django_project/views.py
@@ -1,17 +1,43 @@
-import requests
+
 from django.shortcuts import render
+import requests
+import random
+
+def about(request):
+  # Logic for rendering about.html
+  return render(request, 'about.html')
+
+def contact(request):
+  # Logic for rendering contact.html
+  return render(request, 'contact.html')
 
 def home(request):
-  # USING APIS => Example 1
-  response = requests.get('https://api.github.com/events')
+  # USING APIS create an app that will display the probability it rains in Nairobi
+
+
+  api_key = 'fcdcd146f22bc05fff5200a1e24df0cd'
+  city = 'Nairobi'
+  base_url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+
+  response = requests.get(base_url)
   data = response.json()
-  result = data[0]["repo"]
+
+  result = data.get('rain', {'1h': 'No rain in the next hour'})['1h']
+
+  if result == 'No rain in the next hour':
+      print('Wow!! Look like you can go out and enjoy the weather')
+  else:
+      print('The clouds are gathering. It is raining soon')
+  
+
+
 
   # Example 2
-  reponse2 = requests.get('https://dog.ceo/api/breeds/image/random')
+  reponse2 = requests.get('https://type.fit/api/quotes')
   data2 = reponse2.json()
-  result2 = data2["message"]
+  result2 = random.choice(data2)['text']
 
 
   
   return render(request, 'templates/index.html', {'result': result, 'result2': result2})
+  
